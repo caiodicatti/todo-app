95331fd3c20d8a04b1ca5c935dd4e3a4d53174a9
Caio Dicatti <caio_dicatti@hotmail.com>
2025-06-03 20:41:35 -0300

    chore: add new favicons, refactor CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import TodoEditor from './pages/TodoEditor/TodoEditor';
 import TodoUse from './pages/TodoUse/TodoUse';
 import Home from './pages/Home/Home';
@@ -9,14 +8,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
-    <BrowserRouter basename='todo-app'>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo/:id" element={<TodoEditor />} />
         <Route path="/todo/use/:id" element={<TodoUse />} />
-        <Route path="/todo" element={<TodoEditor />} /> {/* opcional, para criar novo */}
+        <Route path="/todo" element={<TodoEditor />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter >
   )
 }
 
